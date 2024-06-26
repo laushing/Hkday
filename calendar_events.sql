d8b9c6632f22034580d34735fa1feab710487a18
laushing <146660430+laushing@users.noreply.github.com>
2024-06-26 17:39:39 +0800

    Add files via upload

---
+++ calendar_events.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2024-06-26 11:39:08
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `calendar_data`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `calendar_events`
+--
+
+CREATE TABLE `calendar_events` (
+  `event_id` int(11) NOT NULL,
+  `eventName` varchar(255) NOT NULL,
+  `eventDes` text DEFAULT NULL,
+  `eventDate` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `calendar_events`
+--
+ALTER TABLE `calendar_events`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `calendar_events`
+--
+ALTER TABLE `calendar_events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
